8b7bb50611c75319237be4e105edd685331f9f7f
kosteng <2267672@mail.ru>
2019-03-09 22:55:57 +0300

    Загрузка Standart Assets

--- 3DShooter/Assets/Scripts/Interface/ISetHealth.cs
+++ 3DShooter/Assets/Scripts/Interface/ISetHealth.cs
@@ -1,14 +1,8 @@
 ﻿namespace ModelGame
-{
-    /// <summary>
-    /// Интерфейс получения урона
-    /// </summary>
+{ 
     public interface ISetHealth
     {
-        /// <summary>
-        /// Метод получения урона
-        /// </summary>
-        /// <param name="info">Информация о объекте который получит урон</param>
+
         void ApplyHealth(float health);
 
     }
