646645b54a26f5dac882a6bc17f30fa21c732c27
kosteng <2267672@mail.ru>
2019-03-21 20:20:23 +0300

    Работа над освещением

---
+++ 3DShooter/Assets/Scripts/TestAnim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnim : MonoBehaviour
+{
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+            anim.SetFloat("Speed", 1f);
+        
+    }
+}
