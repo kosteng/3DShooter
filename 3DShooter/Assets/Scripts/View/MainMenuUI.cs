b3a2825e95e42fb9a7fa6ed7565588133d6fb0d9
kosteng <2267672@mail.ru>
2019-04-01 19:56:47 +0300

    Добавил звук в главное меню и настройки громкости

---
+++ 3DShooter/Assets/Scripts/View/MainMenuUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+namespace ModelGame 
+{
+	public class MainMenuUI : MonoBehaviour 
+	{
+        [SerializeField] private Slider _slider;
+
+        void Update()
+        {
+            AudioListener.volume = _slider.value;
+        }
+
+
+    }
+}
